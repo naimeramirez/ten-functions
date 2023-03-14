67b2123f3809eff02919b99faf9a9184cdaa78a5
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-03-14 17:33:42 -0600

    ten functions completed

--- solutions.js
+++ solutions.js
@@ -21,3 +21,91 @@
  * concat
  */
 
+//Define a function named isTrue that takes in any input and returns true if the input
+//provided is exactly equal to true in value and data type.
+function isTrue(input){
+    if (input === true){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//Define a function named isFalse that takes in a value and returns a true if and
+//only if the provided input is equal to false in both type and value.
+function isFalse(value){
+    if (value === false){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//Define a function named not that takes in any input and returns the boolean opposite
+//of the provided input.
+function not(input){
+    if (Boolean(input) !== true){
+        return true;
+    }else if (Boolean(input) !== false){
+        return false;
+    }
+}
+
+//Define a function named addOne that takes in a single input. If the input is a
+//number or a numeric string, return the value plus one.
+function addOne(x){
+    var number = Number(x);
+    return number + 1;
+}
+
+//Define a function named isEven that takes in a single input. If the input is an even
+//number or a string containing an even number, return true. Any other input should
+//return false for the output.
+function isEven(x){
+    var number = Number(x);
+    if ((number % 2 === 0) && (number !== true || number !== false)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//Define a function named isIdentical that takes in two input arguments. If each input
+//is equal both in data type and in value, then return true. If the values are not the
+//same data type or not the same value, return false.
+function isIdentical(x, y){
+    if (x === y){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//Define a function named isEqual that takes in two input arguments. If each argument
+//is equal only in value, then return true. Otherwise return false.
+function isEqual(x, y){
+    if (x == y){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//Define a function named or that takes in two input arguments. The output returned
+//should be the result of an or operation on both inputs.
+function or(x, y){
+    return x || y;
+}
+
+//Define a function named and that takes in two input arguments and returns the
+//result of a logical and operation of both inputs.
+function and(x, y){
+    return x && y;
+}
+
+//Define a function named concat that takes in two input arguments. If both arguments
+//are strings, then return the concatenated result. If two numbers are provided, then
+//return the string concatenation of each set of numerals.
+function concat(x, y){
+    return x.toString() + y.toString();
+}
